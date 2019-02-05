76410548283b576f818e84a9a0f596bc2e7847ad
Nic Wainwright <wainwrightnic@gmail.com>
2019-02-05 01:15:50 -0500

    added basic structure of functions to main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Feb 5
+
+@author: nicwainwright
+
+BME547 Homework 4
+Travis CI and Continuous Integration
+"""
+
+def getString():
+    string = input("Please enter your word:\n")
+
+
+def is_tachycardic():
+    string = getString()
+    
+    
+def main():
+    string = is_tachycardic()    
+
+
+if __name__ == "__main__":
+    main()
